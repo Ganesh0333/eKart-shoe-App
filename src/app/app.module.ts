c9576bdda8db15d5fd634b661265e01d532b2c8a
Ganesh0333 <kottaliganesh1994@gmail.com>
2024-07-18 12:03:09 +0530

    header view completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EKartHeaderComponent } from './header/e-kart-header/e-kart-header.component';
+import { MainMenuComponent } from './header/main-menu/main-menu.component';
+import { TopHeaderComponent } from './header/top-header/top-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EKartHeaderComponent
+    EKartHeaderComponent,
+    MainMenuComponent,
+    TopHeaderComponent
   ],
   imports: [
     BrowserModule,
